d13afb361f839ab403662c30c6c98f674551a3b5
Aufa Billah <aufaroot18@gmail.com>
2019-05-06 21:09:58 +0700

    create component app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Form from './components/Form';
+import Recipes from './components/Recipes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// API Key dari food2fork
+const API_KEY = "8bf66259d30db71a84ed6ca7032e7c39";
+
+class App extends Component {
+
+  // membuat state
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    // mengambil value dari input
+    const recipeName = e.target.elements.recipeName.value;
+
+    // melakukan prevent default
+    e.preventDefault();
+
+    // mengakses API dari food2fork
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`) ;
+
+    // mengubah ke format json
+    const data = await api_call.json();
+    
+    // disimpan ke dalam state
+    this.setState({recipes: data.recipes});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes}/>        
+      </div>
+    );
+  }
 }
 
 export default App;
