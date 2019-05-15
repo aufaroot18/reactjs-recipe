1cf58ec537b5599da33a42538192b5e14740041b
Aufa Billah <aufaroot18@gmail.com>
2019-05-16 03:08:42 +0700

    Updates

---
+++ precache-manifest.4c505217392fac79686e8a35284542a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2316d03f72979634f0727a71810f4501",
+    "url": "/reactjs-recipe-app/index.html"
+  },
+  {
+    "revision": "dc61452d76a7c923af30",
+    "url": "/reactjs-recipe-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "efc7735eb40f49ead6d6",
+    "url": "/reactjs-recipe-app/static/css/main.d8c032fe.chunk.css"
+  },
+  {
+    "revision": "dc61452d76a7c923af30",
+    "url": "/reactjs-recipe-app/static/js/2.d3c94571.chunk.js"
+  },
+  {
+    "revision": "efc7735eb40f49ead6d6",
+    "url": "/reactjs-recipe-app/static/js/main.3ebf7242.chunk.js"
+  },
+  {
+    "revision": "a936e324a0532a6cb1a6",
+    "url": "/reactjs-recipe-app/static/js/runtime~main.0819cbf8.js"
+  }
+]);
